3a44ad96e2ae2201c6f6cf3320601bfc32e2dfe9
Patrick Gerken <do3cc@patrick-gerken.de>
2011-10-02 18:17:48 +0200

    Listview aktualisiert

--- src/plonekonf.talk/plonekonf/talk/browser/talk.py
+++ src/plonekonf.talk/plonekonf/talk/browser/talk.py
@@ -3,6 +3,8 @@ from five import grok
 from zope.interface import Interface
 from Products.CMFCore.utils import getToolByName
 
+from plonekonf.talk.interfaces import IVoting
+
 class TalkDefaultView(grok.View):
     grok.context(Interface)
     grok.require('zope2.View')
@@ -19,12 +21,14 @@ class TalkListView(grok.View):
         for brain in talks:
             # Achtung, wir wecken Objekte auf, das kann teuer sein.
             talk = brain.getObject()
+            voting = IVoting(talk)
             yield {
                 # Was passiert bei brain.title?
                 # talk.title
                 'title': brain.Title,
                 # talk.absolute_url()
                 'absolute_url': brain.getURL(),
+                'average_rating': voting.average_vote(),
                 'audience': talk.audience,
                 'uuid': brain.UID,
                 }
